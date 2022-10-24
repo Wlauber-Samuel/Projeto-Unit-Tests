c70ea70acd02665bf4967c7777dbef3d06866cf0
Wlauber <wlaubersamuel@gmail.com>
2022-10-24 09:55:43 -0300

    Resolução do desafio vqv

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,14 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (string, age) => {
+
+if (typeof string === 'string' && typeof age === 'number') {
+  return `Oi, meu nome é ${string}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  }
+};
 
 module.exports = vqv;
